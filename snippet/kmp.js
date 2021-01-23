48785dc42e2d0b856f7d71f6e523f67dcc9f2d2f
hemmingson <287969392@qq.com>
2021-01-23 23:38:56 +0900

    feat: add code snippet
    lovely kmp algorithm

---
+++ snippet/kmp.js
@@ -0,0 +1,33 @@
+const kmp = (s, p) => {
+  const l = p.length,
+    t = Array(l).fill(0)
+
+  {
+    let i = 1,
+      j = 0
+
+    while (i < l) {
+      if (p[i] === p[j]) {
+        t[i++] = ++j
+      } else if (j) j = t[j - 1]
+      else i++
+    }
+  }
+
+  {
+    let i = 0,
+      j = 0
+
+    while (i < s.length) {
+      if (s[i] === p[j]) {
+        i++
+        j++
+      }
+      if (j === l - 1) return true
+      else if (j) j = t[j - 1]
+      else i++
+    }
+  }
+
+  return false
+}
