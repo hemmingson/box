19f4a09aa5251670e09eb717e3691c36eb7821ca
hemmingson <287969392@qq.com>
2021-02-01 20:27:50 +0900

    chore(snippet): linked list implement
    still need to refactor

--- snippet/linkedList.js
+++ snippet/linkedList.js
@@ -47,4 +47,73 @@ function LinkedList() {
 
     length--
   }
+
+  this.isEmpty = function () {
+    return length === 0
+  }
+
+  this.indexOf = function (element) {
+    var currentNode = head
+    var index = -1
+
+    while (currentNode) {
+      index++
+      if (currentNode.element === element) return index
+      currentNode = currentNode.next
+    }
+
+    return -1
+  }
+
+  this.elementAt = function (index) {
+    var currentNode = head
+    var count = 0
+    while (count < index) {
+      count++
+      currentNode = currentNode.next
+    }
+    return currentNode.element
+  }
+
+  this.addAt = function (index, element) {
+    var node = new Node(element)
+
+    var currentNode = head
+    var previousNode
+    var currentIndex = 0
+
+    if (index > length) return false
+
+    if (index === 0) {
+      node.next = currentNode
+      head = node
+    } else {
+      while (currentIndex < index) {
+        currentIndex++
+        previousNode = currentNode
+        currentNode = currentNode.next
+      }
+      node.next = currentNode
+      previousNode.next = node
+    }
+    length++
+  }
+
+  this.removeAt = function (index) {
+    var currentNode = head
+    var previousNode
+    var currentIndex = 0
+    if (index < 0 || index >= length) return null
+    if (index === 0) head = currentNode.next
+    else {
+      while (currentIndex < index) {
+        currentIndex++
+        previousNode = currentNode
+        currentNode = currentNode.next
+      }
+      previousNode.next = currentNode.next
+    }
+    length--
+    return currentNode.element
+  }
 }
