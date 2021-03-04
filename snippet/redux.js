6c8e51addad5b03b7e6a844d79f45758a891a4df
hemmingson <287969392@qq.com>
2021-03-04 22:25:01 +0900

    feat(snippet): add redux file
    add new feature awesome redux

---
+++ snippet/redux.js
@@ -0,0 +1,7 @@
+/**
+ * ==================
+ * awesome redux ⚛️🧳
+ * ==================
+ * [dan abramov] https://egghead.io/courses/fundamentals-of-redux-course-from-dan-abramov-bd5cc867
+ * [lin clark] https://code-cartoons.com/
+ */
