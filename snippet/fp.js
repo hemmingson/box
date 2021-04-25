140c30d8d3fbd67c9e1d4a1003094a8f39ac1756
hemmingson <287969392@qq.com>
2021-04-25 16:05:33 +0900

    chore(snippet): supplement fp snippet
    add curry function

--- snippet/fp.js
+++ snippet/fp.js
@@ -1,3 +1,16 @@
 // λ functional programming 💛
 
 const compose = (...fns) => (x) => fns.reduceRight((acc, f) => f(acc), x)
+
+const curry = (f) => {
+  let params = []
+
+  const next = (...args) => {
+    params = [...params, ...args]
+
+    if (params.length < f.length) return next
+    return f.apply(f, params)
+  }
+
+  return next
+}
