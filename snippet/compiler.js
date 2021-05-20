0ec1766e4f2b9022708f33cca523a9fc75429239
hemmingson <287969392@qq.com>
2021-05-20 07:45:28 -0700

    chore(snippet): supplement compiler snippet

--- snippet/compiler.js
+++ snippet/compiler.js
@@ -81,7 +81,7 @@ const tokenizer = (x) => {
         c = x[++cur]
       }
 
-      tokens.push({ type: 'content', value })
+      tokens.push({ type: 'name', value })
 
       continue
     }
@@ -96,41 +96,44 @@ const parser = (x) => {
   let cur = 0
 
   const recursion = () => {
-    let { type, value } = x[cur]
+    let token = x[cur]
 
-    if (type === 'numeric') {
+    if (token.type === 'numeric') {
       ++cur
 
       return {
         type: 'NumericLiteral',
-        value,
+        value: token.value,
       }
     }
 
-    if (type === 'string') {
+    if (token.type === 'string') {
       ++cur
 
       return {
         type: 'StringLiteral',
-        value,
+        value: token.value,
       }
     }
 
-    if (type === 'parenthesis' && value === '(') {
-      token = tokens[++cur]
+    if (token.type === 'parenthesis' && token.value === '(') {
+      token = x[++cur]
 
       let node = {
         type: 'CallExpression',
-        content: token.value,
+        name: token.value,
         params: [],
       }
 
-      token = tokens[++cur]
+      token = x[++cur]
 
-      while (type !== 'parenthesis') {
+      while (
+        token.type !== 'parenthesis' ||
+        (token.type === 'parenthesis' && token.value !== ')')
+      ) {
         node.params.push(recursion())
 
-        token = tokens[cur]
+        token = x[cur]
       }
 
       ++cur
@@ -139,5 +142,138 @@ const parser = (x) => {
     }
 
     throw new TypeError(token.type)
+  }
+
+  const ast = {
+    type: 'Program',
+    body: [],
+  }
+
+  while (cur < x.length) {
+    ast.body.push(recursion())
+  }
+
+  return ast
+}
+
+const traverser = (ast, visitor) => {
+  const traverseArray = (arr, parent) => {
+    arr.forEach((el) => {
+      traverseNode(el, parent)
+    })
+  }
+
+  const traverseNode = (node, parent) => {
+    const methods = visitor[node.type]
+
+    if (methods && methods.enter) {
+      methods.enter(node, parent)
+    }
+
+    switch (node.type) {
+      case 'Program':
+        traverseArray(node.body, node)
+        break
+
+      case 'CallExpression':
+        traverseArray(node.params, node)
+        break
+
+      case 'NumericLiteral':
+
+      case 'StringLiteral':
+        break
+
+      default:
+        throw new TypeError(node.type)
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent)
+    }
+  }
+
+  traverseNode(ast, null)
+}
+
+const transformer = (ast) => {
+  const dummy = {
+    type: 'Program',
+    body: [],
+  }
+
+  ast._context = dummy.body
+
+  traverser(ast, {
+    NumericLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'NumericLiteral',
+          value: node.value,
+        })
+      },
+    },
+
+    StringLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'StringLiteral',
+          value: node.value,
+        })
+      },
+    },
+
+    CallExpression: {
+      enter(node, parent) {
+        let expression = {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: node.name,
+          },
+          arguments: [],
+        }
+
+        node._context = expression.arguments
+
+        if (parent.type !== 'CallExpression') {
+          expression = {
+            type: 'ExpressionStatement',
+            expression,
+          }
+        }
+
+        parent._context.push(expression)
+      },
+    },
+  })
+
+  return dummy
+}
+
+const generator = (node) => {
+  switch (node.type) {
+    case 'Program':
+      return node.body.map(generator).join('\n')
+
+    case 'ExpressionStatement':
+      return generator(node.expression) + ';'
+
+    case 'CallExpression':
+      return `${generator(node.callee)}(${node.arguments
+        .map(generator)
+        .join(', ')})`
+
+    case 'Identifier':
+      return node.name
+
+    case 'NumericLiteral':
+      return node.value
+
+    case 'StringLiteral':
+      return `$[node.value]`
+
+    default:
+      throw new TypeError(node.type)
   }
 }
